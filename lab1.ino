71a56da4586c65db972d63a93b64aedafa6ba4ae
chadBrobot <62157388+chadBrobot@users.noreply.github.com>
2020-03-19 19:38:33 -0600

    Add files via upload

---
+++ lab1.ino
@@ -0,0 +1,28 @@
+#include <AFMotor.h>
+#include <math.h> 
+//set up for motor and speeds
+AF_DCMotor motor_left(1); //pin 11
+AF_DCMotor motor_right(2); //pin 3
+
+int speed_right = 125;
+
+int speed_left = (30 + (0.435*speed_right)) + (0.00152 * (speed_right * speed_right));// forward formula   
+//int speed_left = (-391 + (102 * log(speed_right)));//backward formula
+
+void setup() {
+  delay(2000);
+  Serial.begin(9600);           
+  motor_left.setSpeed(speed_left);
+  motor_right.setSpeed(speed_right);
+  motor_left.run(RELEASE);
+  motor_right.run(RELEASE); 
+}
+
+
+void loop() {
+  motor_left.run(FORWARD);
+  motor_right.run(FORWARD);
+  delay(2000);
+  motor_left.run(RELEASE);
+  motor_right.run(RELEASE);
+}
